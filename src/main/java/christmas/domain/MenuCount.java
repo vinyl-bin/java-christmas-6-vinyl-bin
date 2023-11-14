1fccc3af77a43778c46175dc4a55149a5986db8e
dabin <db021129@naver.com>
2023-11-14 15:20:08 +0900

    docs : add MenuCount

--- src/main/java/christmas/domain/MenuCount.java
+++ src/main/java/christmas/domain/MenuCount.java
@@ -1,4 +1,22 @@
 package christmas.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MenuCount {
+    private final List<Menu> menus;
+    private final List<Integer> counts;
+
+    public MenuCount(List<Menu> menus, List<Integer> counts) {
+        this.menus = menus;
+        this.counts = counts;
+    }
+
+    public List<Menu> getMenus() {
+        return menus;
+    }
+
+    public List<Integer> getCounts() {
+        return counts;
+    }
 }
