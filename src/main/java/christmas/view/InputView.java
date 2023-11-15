bd403a89d80f1b637498c45de6c6f18f03b6e569
dabin <db021129@naver.com>
2023-11-15 13:12:10 +0900

    feat : create readMenu

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -3,6 +3,7 @@ package christmas.view;
 import camp.nextstep.edu.missionutils.Console;
 import christmas.controller.InputViewController;
 import christmas.domain.DateInfo;
+import christmas.domain.MenuCount;
 
 public class InputView {
 
@@ -12,5 +13,12 @@ public class InputView {
         String input = Console.readLine();
         DateInfo dateInfo = inputViewController.connectDateInfo(input);
         return dateInfo;
+    }
+
+    public MenuCount readMenu() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = Console.readLine();
+        MenuCount menuCount = inputViewController.connectMenuCount(input);
+        return menuCount;
     }
 }
