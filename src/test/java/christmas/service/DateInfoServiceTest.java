934baf152efe6701d7d7d97e992cd79c93bc47a5
dabin <db021129@naver.com>
2023-11-15 23:11:09 +0900

    feat : create DateInfoServiceTest

---
+++ src/test/java/christmas/service/DateInfoServiceTest.java
@@ -0,0 +1,17 @@
+package christmas.service;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DateInfoServiceTest extends NsTest {
+
+    @Test
+    void DateInfo_예외_처리() {
+        assertSimpleTest(() -> {
+            runException("a");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        });
+    }
+}
